e8c205d54ae3b286ae09437f5fca10123435191f
markkulibaba <mlsmarikpro@icloud.com>
2026-01-27 16:04:42 -0800

    loaded enemy(test)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { GameEngine } from "./gameengine.js";
 import { AssetManager } from "./assetmanager.js";
 import Player from "./src/actors/player.js";
+import Enemy from "./src/actors/enemy.js";
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
@@ -11,8 +12,12 @@ ASSET_MANAGER.downloadAll(() => {
 
 	gameEngine.init(ctx);
 
+
 	const player = new Player(gameEngine, 100, 500);
 	gameEngine.addEntity(player);
+
+	const enemy = new Enemy(gameEngine, 300, 500);
+	gameEngine.addEntity(enemy);
 
 	gameEngine.start();
 });
